b86958073ffe06214019bf256bb254aa9b87abff
Tamim Ahmed <78292363+tendulkar360@users.noreply.github.com>
2021-04-13 22:41:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// const dropdown = document.getElementById("dropdown");
+// const drop = document.getElementById("drop");
+// const dropdownIcon = document.getElementById("dropdown-icon");
+
+// dropdown.addEventListener("click" , function(){
+//     drop.style = "transform: translateY(0px); opacity: 1; color: black;";
+//     dropdownIcon.stye = "transform: rotateZ(0deg);";
+// })
